0f6ba77de18e2664b2709ee8fcac9a63d0c2910f
20161104598 <1124535694@qq.com>
2018-09-26 11:08:53 +0800

    1
    2

--- clmtest/ViewController.swift
+++ clmtest/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(_ sender: Any) {
+        z.text = "\(Double(x.text!)! + Double(y.text!)!)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
